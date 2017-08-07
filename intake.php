dd1dcaa2716e254c7f001fff7dc1f02deaa15f19
Al Crowley <albertcrowley@gmail.com>
2017-08-07 04:34:25 +0000

    Work in progress

--- intake.php
+++ intake.php
@@ -3,7 +3,9 @@ require_once 'vendor/autoload.php';
 use Screen\Capture;
 use Jenssegers\ImageHash\ImageHash;
 use claviska\SimpleImage;
-use mtghash\Card;
+use keboola\csv;
+
+require_once "mtghash/Card.php";
 
 $file_db = new PDO('sqlite:cards.sqlite3');
 // Set errormode to exceptions
@@ -31,15 +33,52 @@ function addToDB($title, $all_hash, $art_hash) {
 }
 
 
+echo "<pre>";
+
 setupDB();
-$allfiles = glob("cards/old_core_v1/*.jp*g");
-foreach ($allfiles as $file) {
-    $hashes = Card::hashCard($file);
-    echo "$file ${hashes['all_hash']} ${hashes['art_hash']} <br>";
-    addToDB($file, $hashes['all_hash'], $hashes['art_hash']);
+
+$card_array = array();
+$csv_file = new Keboola\Csv\CsvFile("./cards.csv");
+foreach ($csv_file as $row) {
+  if ($row[0] == null || trim($row[0]) == "" || $row[0] == "name") {
+    continue;
+  }
+  $a = array();
+  $a['name'] = $row[0];
+  $a['set'] = $row[2];
+  $a['set_code'] = $row[4];
+  $a['id'] = $row[6];
+  $a['type'] = $row[8];
+  $a['power'] = $row[10];
+  $a['toughness'] = $row[12];
+  $a['manacost'] = $row[16];
+  $a['converted_manacost'] = $row[18];
+  $a['artist'] = $row[20];
+  $a['flavor'] = $row[22];
+  $a['color'] = $row[24];
+  $a['number'] = $row[28];
+  $a['rarity'] = $row[30];
+  $a['number_int'] = $row[54];
+  $card_array[] = $a;
+}
+
+
+echo "Read " . count($card_array) . " cards.";
+foreach ($card_array as $a) {
+
+  $card = new \mtghash\Card($a['name'], $a['set_code'], $a['id'],$a['number_int'] );
+    $hashes = $card->hash();
+    if ($hashes != null) {
+      echo "${a['name']} (${a['id']}) hashes to: all-${hashes['all_hash']} art-${hashes['art_hash']}" . PHP_EOL;
+    } else {
+      echo "${a['set_code']} ${a['name']} (${a['id']}) - IMAGE/HASH NOT FOUND " . PHP_EOL;
+    }
+  echo $card->getImageFile(true) . PHP_EOL;
+    //addToDB($file, $hashes['all_hash'], $hashes['art_hash']);
     flush();
 }
-echo "<pre>";
+
+//print_r($card_array);
 
 $sth = $file_db->prepare ("select * from cards");
 $sth->execute();
--- mtghash/Card.php
+++ mtghash/Card.php
@@ -10,11 +10,63 @@ namespace mtghash;
 
 
 class Card {
-  static $ART_COORDS = [40,50,430,370];
+  static $ART_COORDS = [23,28,237,232];
   static $X1 = 0;
   static $Y1 = 1;
   static $X2 = 2;
   static $Y2 = 3;
+  public $name = "";
+  public $set_code = "";
+  public $id = "";
+  public $in_set_number= "";
+
+  public function __construct($name, $set_code, $id_num, $in_set_number) {
+    $this->name = $name;
+    $this->set_code = $set_code;
+    $this->id = $id_num;
+    $this->in_set_number = $in_set_number;
+  }
+
+  public function hash() {
+    $img_file = $this->getImageFile();
+    if ($img_file != null){
+      return self::hashImage($img_file);
+    }
+    return null;
+
+  }
+
+  public function getImageFile($force = false) {
+    $root = __DIR__ . DIRECTORY_SEPARATOR .
+      ".." . DIRECTORY_SEPARATOR .
+      "cards" . DIRECTORY_SEPARATOR .
+      $this->set_code . DIRECTORY_SEPARATOR;
+    $file = $root . $this->id . " - " . $this->name . " - " . $this->getExtendedInSetNumber() . ".full.jpg";
+
+    if (!file_exists($file)) {
+      // try finding just by card number
+      $files = glob($root . $this->id . "*");
+      if (count($files) > 0) {
+        $file = $files[0];
+      } else {
+        //try just by name
+        $files = glob($root . "*" . $this->name . "*");
+        if (count($files) > 0) {
+          $file = $files[0];
+        }
+      }
+    }
+
+    if ($force || file_exists($file)) {
+      return $file;
+    }
+    return null;
+  }
+
+  public function getExtendedInSetNumber() {
+    $n = sprintf("%'.03d", $this->in_set_number);
+    return $n;
+  }
 
   /***
    * Hashes the image of a card
@@ -23,31 +75,32 @@ class Card {
    *
    * @return array ("all_hash"=>XXX, "art_hash"=>YYY)
    */
-  public static function hashCard($file) {
+  public static function hashImage($file) {
 
 
-    $root = "/var/www/mtghash/";
-    //$in = "./cards/Mana Flare.full.jpg";
-    $in = $file;
     $out = tempnam(sys_get_temp_dir(), "mtghash-card-");
     $artfile = tempnam(sys_get_temp_dir(), "mtghash-card-art-");
-    $image = new claviska\SimpleImage();
-    $image->fromFile($root . $in)
-      ->toFile($root . $out);
+    $image = new \claviska\SimpleImage($file);
+    $image->fromFile($file)
+      ->toFile($out);
 
-    $image->crop(Card::$ART_COORDS_PCT[Card::$X1], Card::$ART_COORDS_PCT[Card::$Y1]
-      , Card::$ART_COORDS_PCT[Card::$X2], Card::$ART_COORDS_PCT[Card::$Y2]
-    )->toFile($root . $artfile);
+    $image->crop(self::$ART_COORDS [self::$X1], self::$ART_COORDS[self::$Y1]
+      , self::$ART_COORDS[self::$X2], self::$ART_COORDS[self::$Y2]
+    )->toFile($artfile);
 
     $algs = [
-      new Jenssegers\ImageHash\Implementations\AverageHash(),
-      new Jenssegers\ImageHash\Implementations\DifferenceHash(),
-      new Jenssegers\ImageHash\Implementations\PerceptualHash()
+      new \Jenssegers\ImageHash\Implementations\AverageHash(),
+      new \Jenssegers\ImageHash\Implementations\DifferenceHash(),
+      new \Jenssegers\ImageHash\Implementations\PerceptualHash()
     ];
 
-    $hasher = new ImageHash($algs[0]);
+    $hasher = new \Jenssegers\ImageHash\ImageHash($algs[1]);
     $all_hash = $hasher->hash($out);
+    print ("got $all_hash" . PHP_EOL);
     $art_hash = $hasher->hash($artfile);
+
+//    unlink($artfile);
+//    unlink($out);
 
     return array("all_hash" => $all_hash, "art_hash" => $art_hash);
   }
